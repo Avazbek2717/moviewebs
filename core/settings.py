e5dc01c77c0b3bd8c0dff4361553b5e034d2aca2
Avazbek <ax1of710@gmail.com>
2025-01-16 19:40:20 +0500

    last ch

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG')))
 
-print(bool(int(os.getenv('DEBUG'))))
+
 
 ALLOWED_HOSTS = ["*"]
 
